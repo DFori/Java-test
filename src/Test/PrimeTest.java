1cbc1e88498f4f50d4819ff0925d6a3a641e49b5
Daniel Fori <122609602+DFori@users.noreply.github.com>
2024-11-14 20:02:00 +0100

    edited PrimeTest.java

--- src/Test/PrimeTest.java
+++ src/Test/PrimeTest.java
@@ -5,7 +5,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PrimeTest {
     @Test
     public void testIsPrime() {
-        assertTrue(isPrime(7));
+        assertTrue(isPrime(1));
+        assertTrue(isPrime(2));
+        assertTrue(isPrime(3));
+        assertTrue(isPrime(4));
     }
 
     public boolean isPrime(int number) {
